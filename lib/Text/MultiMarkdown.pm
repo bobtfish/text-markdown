3e6ca1d8410eb23b1299fdc88dd92231276f7974
bobtfish <bobtfish@4b3c33cd-4c29-0410-a07e-8d9eac594458>
2008-11-02 19:49:04 +0000

    Checking in changes prior to tagging of version 1.0.23.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 8205)
    +++ Changes	(local)
    @@ -195,4 +195,7 @@
             - RT#40303 - Fix use in Mason by adding "use re 'eval'" to both files.
             - Add SVN repository to Makefile.PL
             - Remove META.yml from source control so it is regenerated 
    -          automatically.
    \ No newline at end of file
    +          automatically.
    +          
    +1.0.23 2008-11-02T18:24:30
    +        - Ship a release with the correct contents so that the tests pass.

--- lib/Text/MultiMarkdown.pm
+++ lib/Text/MultiMarkdown.pm
@@ -9,7 +9,7 @@ use Encode      qw();
 use Carp        qw(croak);
 use base        qw(Text::Markdown);
 
-our $VERSION   = '1.0.22';
+our $VERSION   = '1.0.23';
 our @EXPORT_OK = qw(markdown);
 
 =head1 NAME
